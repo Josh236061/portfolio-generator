e79fa163fd0d6d869b5a24799ab97e8ab70c4c42
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-05 13:27:11 -0500

    modularized HTML file & lesson 2 finalizations

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const fs = require('fs');
+const fs = require('fs');;
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require ('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
